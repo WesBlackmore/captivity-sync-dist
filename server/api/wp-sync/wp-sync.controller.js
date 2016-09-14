008e693d8396337577ff6755940fa327a28b650a
Wes Blackmore <wes.blackmore@gmail.com>
2016-09-14 13:22:29 +0200

    Latest Deploy

--- server/api/wp-sync/wp-sync.controller.js
+++ server/api/wp-sync/wp-sync.controller.js
@@ -18,7 +18,7 @@ exports.sync = function(request, result) {
 
 	var options = {
 		host: 'www.captivity.co.za',
-		path: '/wc-api/v3/products?filter[limit]=-1&consumer_key=' + consumer_key + '&consumer_secret=' + consumer_secret,
+		path: '/wc-api/v3/products?filter[limit]=10&consumer_key=' + consumer_key + '&consumer_secret=' + consumer_secret,
 		headers: {
 			"Content-Type": "application/json"
 		},
